dfca942930b157727be2f24da76d3eb288aded93
Sam Cutler <sam.cutler@gmail.com>
2021-01-11 14:40:27 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import { Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+
+    return (
+      <span>
+        <h1>This is the start of HikeTracker</h1>
+      </span>
+    )
+
+  }
+
 }
 
 export default App;
