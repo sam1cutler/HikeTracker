b9c7b22ddc931764ddbb0964d2a8b2556fb7dc0d
Sam Cutler <sam.cutler@gmail.com>
2021-01-22 17:35:48 -0800

    A few pre-deployment tweaks

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    //PORT: process.env.PORT || 8000,
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
     TOKEN_KEY: 'hike-tracker-client-auth-token',
-}
+};
 
 export default config;
