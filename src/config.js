f9ce58f1f881d408d9c2216028e2d4deeb22267a
Sam Cutler <sam.cutler@gmail.com>
2021-01-23 17:31:26 -0800

    Set API endpoint to Heroku URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    //PORT: process.env.PORT || 8000,
     //API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
-    API_ENDPOINT: 'https://glacial-meadow-22221.herokuapp.com/',
+    //API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://glacial-meadow-22221.herokuapp.com/api',
     TOKEN_KEY: 'hike-tracker-client-auth-token',
 };
 
